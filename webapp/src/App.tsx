cb6014db2713146c6916142ae78949cb0246a618
Artem Tarakanovskiy <piratetigo@gmail.com>
2024-11-08 11:33:09 +0700

    Added router

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,8 +1,15 @@
 import { TrpcProvider } from '@share/component'
 import { type FC } from 'react'
+import { BrowserRouter, Routes } from 'react-router-dom'
 
 export const App: FC = () => {
-  return <TrpcProvider></TrpcProvider>
+  return (
+    <TrpcProvider>
+      <BrowserRouter>
+        <Routes></Routes>
+      </BrowserRouter>
+    </TrpcProvider>
+  )
 }
 
 export default App
