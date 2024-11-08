9c7e4ab5216bf77575e1471af729f013127038bf
Artem Tarakanovskiy <piratetigo@gmail.com>
2024-11-08 12:44:15 +0700

    Added styles: sass, stylelint

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -2,6 +2,8 @@ import { TrpcProvider } from '@share/component'
 import { type FC } from 'react'
 import { BrowserRouter, Routes } from 'react-router-dom'
 
+import './styles/global.scss'
+
 export const App: FC = () => {
   return (
     <TrpcProvider>
