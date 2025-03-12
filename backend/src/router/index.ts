f604b06a023c85d684b8957c7eba006b1db00718
Artem Tarakanovskiy <piratetigo@gmail.com>
2025-03-12 22:46:22 +0700

    Added index generation

--- backend/src/router/index.ts
+++ backend/src/router/index.ts
@@ -1,5 +1,11 @@
 import { trpc } from '../lib/trpc'
+// @index('./**/index.ts', f => `import { ${f.path.split('/').slice(0, -1).pop()}TrpcRoute } from '${f.path.split('/').slice(0, -1).join('/')}'`)
 
-export const trpcRouter = trpc.router({})
+// @endindex
+
+export const trpcRouter = trpc.router({
+  // @index('./**/index.ts', f => `${f.path.split('/').slice(0, -1).pop()}: ${f.path.split('/').slice(0, -1).pop()}TrpcRoute,`)
+  // @endindex
+})
 
 export type TrpcRouter = typeof trpcRouter
